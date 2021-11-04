999c3fd15dbf6efa617935179925cca5190353fb
RomanieDelporte <romadelp@student.arteveldehs.be>
2021-11-04 15:17:01 +0100

    add something

--- romaniedelporte/src/app/pages/HomePage.js
+++ romaniedelporte/src/app/pages/HomePage.js
@@ -1,6 +1,7 @@
 /** @format */
 
 import React from "react";
+import "animate.css";
 import mezelf from "../assets/images/mezelf.jpg";
 
 const HomePage = () => {
@@ -11,7 +12,7 @@ const HomePage = () => {
 					<div className=' homepage_image col-md-6'>
 						<img src={mezelf} alt='pic' />
 					</div>
-					<div className='homepage_intro col-md-6 '>
+					<div className='homepage_intro col-md-6  '>
 						<h1>
 							<span>I'm</span> a front-end/Full-stack Developer
 						</h1>
@@ -20,51 +21,9 @@ const HomePage = () => {
 							great code.{" "}
 						</h3>
 					</div>
-
-					<div className='col-md-4'>
-						<div className='homepage_info mt-5'>
-							<h2>Tools</h2>
-							<div className='homepage_skills mt-5 pb-3 pt-4 m-5'>
-								<ul>
-									<li>Visual studio code</li>
-									<li>Github</li>
-									<li>Terminal</li>
-									<li>Adobe</li>
-								</ul>
-							</div>
-						</div>
-					</div>
-					<div className='col-md-4'>
-						<div className='homepage_info mt-5'>
-							<h2>Skills</h2>
-							<div className='homepage_skills mt-5 pb-3 pt-4 m-5'>
-								<ul>
-									<li>Javasript(es6) </li>
-									<li>Html5</li>
-									<li>CSS(sass,scss)</li>
-									<li>React</li>
-									<li>React native</li>
-									<li>Svelte</li>
-									<li>Wordpress</li>
-									<li>Graphql</li>
-									<li>Apollo Client</li>
-								</ul>
-								<p>See more skills</p>
-							</div>
-						</div>
-					</div>
-					<div className='col-md-4'>
-						<div className='homepage_info mt-5'>
-							<h2>Knowledge</h2>
-							<div className='homepage_skills mt-5 pb-3 pt-4 m-5'>
-								<ul>
-									<li>Redux </li>
-									<li>Typescript</li>
-									<li>Php</li>
-									<li>Three.js</li>
-								</ul>
-							</div>
-						</div>
+					<div className='homepage_myself mt-5'>
+						<h1>About me</h1>
+						<span></span>
 					</div>
 				</div>
 			</div>
